d518a99a473e907c918a5ee385ba7ca2786ba979
Пономарев Дмитрий Александрович <da_ponomarev@bingo-boom.com>
2020-08-24 10:51:58 +0300

    Updated comments for methods

--- logger.go
+++ logger.go
@@ -43,7 +43,11 @@ var levelColors = map[string]string{
 }
 
 // Logger settings
-var loggerCfg cfg
+var loggerCfg = cfg{
+	appName: "APP",
+	logPath: "",
+	level:   0,
+}
 
 // bWriter - used to output logs to a file
 var bWriter *bufio.Writer
@@ -59,8 +63,10 @@ type cfg struct {
 
 /*
 Debug - category of logs, used for debugging code.
-	lc <string> - logging category
-	text <string> - text of the log's message
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
 */
 func Debug(lc string, text string) {
 	if loggerCfg.level > 0 {
@@ -72,8 +78,10 @@ func Debug(lc string, text string) {
 
 /*
 Info - the category of logs used for information messages in the algorithm.
-	lc <string> - logging category
-	text <string> - text of the log's message
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
 */
 func Info(lc string, text string) {
 	if loggerCfg.level > 1 {
@@ -85,8 +93,10 @@ func Info(lc string, text string) {
 
 /*
 Warning - the category of logs used to display warnings of the program logic.
-	lc <string> - logging category
-	text <string> - text of the log's message
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
 */
 func Warning(lc string, text string) {
 	if loggerCfg.level > 2 {
@@ -98,8 +108,10 @@ func Warning(lc string, text string) {
 
 /*
 Error - a category of logs used to display errors in the program logic.
-	lc <string> - logging category
-	text <string> - text of the log's message
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
 */
 func Error(lc string, text string) {
 	if loggerCfg.level > 3 {
@@ -111,8 +123,10 @@ func Error(lc string, text string) {
 
 /*
 Critical - a category of logs used to display fatal errors in the event of which the program terminates.
-	lc <string> - logging category
-	text <string> - text of the log's message
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
 */
 func Critical(lc string, text string) {
 	if loggerCfg.level > 4 {
@@ -122,6 +136,15 @@ func Critical(lc string, text string) {
 	logManage("CRITICAL", lc, text)
 }
 
+/*
+logManage - used to manage the display of logs.
+
+- level <string> - level of logs
+
+- lc <string> - logging category
+
+- text <string> - text of error
+*/
 func logManage(level string, lc string, text string) {
 	if bWriter == nil {
 		fmt.Println(makeLogString(level, lc, text))
@@ -132,13 +155,26 @@ func logManage(level string, lc string, text string) {
 	bWriter.Flush()
 }
 
-func makeLogString(level string, lc string, message string) string {
+/*
+makeLogString - used to create a formatted log string.
+
+- level <string> - level of logs
+
+- lc <string> - logging category
+
+- text <string> - text of error
+
+*/
+func makeLogString(level string, lc string, text string) string {
 	now := time.Now().Format(timeFormat)
 	logPos := getLogPosition()
 	return fmt.Sprint(levelColors[level],
-		"[", loggerCfg.appName, "]", "[", now, "]", "[", lc, "]", "[", logPos, "]", "[", level, "]  ▶  ", message)
+		"[", loggerCfg.appName, "]", "[", now, "]", "[", lc, "]", "[", logPos, "]", "[", level, "]  ▶  ", text)
 }
 
+/*
+getLogPosition - used to determine where the log is called.
+*/
 func getLogPosition() string {
 	_, file, line, _ := runtime.Caller(4)
 
@@ -147,9 +183,13 @@ func getLogPosition() string {
 
 /*
 SetConfig - configures the logging module for further work.
-	appName <string> - name of the program.
-	logPath <string> - path of the file for writing logs to it. If the variable is empty, then logs are not written to the file.
-	level <int> - logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
+
+- appName <string> - name of the program.
+
+- logPath <string> - path of the file for writing logs to it.
+If the variable is empty, then logs are not written to the file.
+
+- level <int> - logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
 */
 func SetConfig(appName string, logPath string, level int) {
 	loggerCfg.appName = appName
@@ -159,11 +199,10 @@ func SetConfig(appName string, logPath string, level int) {
 	checkConfig()
 }
 
+/*
+checkConfig - checks the config and reconfigures it if necessary.
+*/
 func checkConfig() {
-	if len(loggerCfg.appName) < 1 {
-		loggerCfg.appName = "APP"
-	}
-
 	if loggerCfg.level < 0 || loggerCfg.level > 4 {
 		loggerCfg.level = 0
 	}
