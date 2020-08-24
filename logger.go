ba6918474c377389bfe45c993680b9f0a9ce632b
Пономарев Дмитрий Александрович <da_ponomarev@bingo-boom.com>
2020-08-24 14:34:51 +0300

    Added methods for displaying JSON logs

--- logger.go
+++ logger.go
@@ -2,10 +2,13 @@ package logger
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	s "strings"
 	"time"
 )
 
@@ -26,6 +29,7 @@ const (
 	fgRed         string = "\x1b[31m"
 	fgGreen       string = "\x1b[32m"
 	fgYellow      string = "\x1b[33m"
+	fgPurple      string = "\033[35m"
 	fgBlue        string = "\x1b[34m"
 	fgMagenta     string = "\x1b[35m"
 	fgCyan        string = "\x1b[36m"
@@ -39,26 +43,62 @@ var levelColors = map[string]string{
 	"INFO":     fgGreen,
 	"WARNING":  fgYellow,
 	"ERROR":    fgRed,
-	"CRITICAL": fgBoldRed,
+	"CRITICAL": fgPurple,
 }
 
 // Logger settings
-var loggerCfg = cfg{
-	appName: "APP",
-	logPath: "",
-	level:   0,
+var loggerCfg = Cfg{
+	AppName: "APP",
+	LogPath: "",
+	Level:   0,
 }
 
 // bWriter - used to output logs to a file
 var bWriter *bufio.Writer
 
 /*
-cfg - a structure that stores the settings of the logging module
+Cfg - a structure that stores the settings of the logging module
+	- Cfg - contains:
+
+		- AppName <string> - name of the program.
+
+		- LogPath <string> - path of the file for writing logs to it.
+		If the variable is empty, then logs are not written to the file.
+
+		- Level <int> - logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
 */
-type cfg struct {
-	appName string // name of the program.
-	logPath string // path of the file for writing logs to it.
-	level   int    // logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
+type Cfg struct {
+	AppName string
+	LogPath string
+	Level   int
+}
+
+/*
+JSONLog - structure of a log message in JSON format
+	- JSONLog - contains:
+
+		- AppName <string> - name of the program.
+
+		- Time <string> - log's time.
+
+		- LC <string> - logging category
+
+		- File <string> - file name where log is called.
+
+		- Line <string> - line where log is called.
+
+		- Level <string> - logger level
+
+		- Text <string> - log's text
+*/
+type JSONLog struct {
+	AppName string `json:"app_name"`
+	Time    string `json:"date"`
+	LC      string `json:"lc"`
+	File    string `json:"file"`
+	Line    int    `json:"line"`
+	Level   string `json:"level"`
+	Text    string `json:"text"`
 }
 
 /*
@@ -69,11 +109,26 @@ Debug - category of logs, used for debugging code.
 - text <string> - text of the log's message
 */
 func Debug(lc, text string) {
-	if loggerCfg.level > 0 {
+	if loggerCfg.Level > 0 {
 		return
 	}
 
 	logManage("DEBUG", lc, text)
+}
+
+/*
+DebugJ - category of JSON logs, used for debugging code.
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
+*/
+func DebugJ(lc, text string) {
+	if loggerCfg.Level > 0 {
+		return
+	}
+
+	logJSONManage("DEBUG", lc, text)
 }
 
 /*
@@ -84,11 +139,26 @@ Info - the category of logs used for information messages in the algorithm.
 - text <string> - text of the log's message
 */
 func Info(lc, text string) {
-	if loggerCfg.level > 1 {
+	if loggerCfg.Level > 1 {
 		return
 	}
 
 	logManage("INFO", lc, text)
+}
+
+/*
+InfoJ - category of JSON logs, used for debugging code.
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
+*/
+func InfoJ(lc, text string) {
+	if loggerCfg.Level > 0 {
+		return
+	}
+
+	logJSONManage("INFO", lc, text)
 }
 
 /*
@@ -99,11 +169,26 @@ Warning - the category of logs used to display warnings of the program logic.
 - text <string> - text of the log's message
 */
 func Warning(lc, text string) {
-	if loggerCfg.level > 2 {
+	if loggerCfg.Level > 2 {
 		return
 	}
 
 	logManage("WARNING", lc, text)
+}
+
+/*
+WarningJ - category of JSON logs, used for debugging code.
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
+*/
+func WarningJ(lc, text string) {
+	if loggerCfg.Level > 0 {
+		return
+	}
+
+	logJSONManage("WARNING", lc, text)
 }
 
 /*
@@ -114,11 +199,26 @@ Error - a category of logs used to display errors in the program logic.
 - text <string> - text of the log's message
 */
 func Error(lc, text string) {
-	if loggerCfg.level > 3 {
+	if loggerCfg.Level > 3 {
 		return
 	}
 
 	logManage("ERROR", lc, text)
+}
+
+/*
+ErrorJ - category of JSON logs, used for debugging code.
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
+*/
+func ErrorJ(lc, text string) {
+	if loggerCfg.Level > 0 {
+		return
+	}
+
+	logJSONManage("ERROR", lc, text)
 }
 
 /*
@@ -129,11 +229,45 @@ Critical - a category of logs used to display fatal errors in the event of which
 - text <string> - text of the log's message
 */
 func Critical(lc, text string) {
-	if loggerCfg.level > 4 {
+	if loggerCfg.Level > 4 {
 		return
 	}
 
 	logManage("CRITICAL", lc, text)
+}
+
+/*
+CriticalJ - category of JSON logs, used for debugging code.
+
+- lc <string> - logging category
+
+- text <string> - text of the log's message
+*/
+func CriticalJ(lc, text string) {
+	if loggerCfg.Level > 0 {
+		return
+	}
+
+	logJSONManage("CRITICAL", lc, text)
+}
+
+/*
+logJSONManage - used to manage the display of JSON logs.
+
+- level <string> - level of logs
+
+- lc <string> - logging category
+
+- text <string> - text of error
+*/
+func logJSONManage(level, lc, text string) {
+	if bWriter == nil {
+		fmt.Println(makeLogJSONString(level, lc, text))
+		return
+	}
+
+	fmt.Fprintf(bWriter, "%s\n", makeLogJSONString(level, lc, text))
+	bWriter.Flush()
 }
 
 /*
@@ -163,13 +297,43 @@ makeLogString - used to create a formatted log string.
 - lc <string> - logging category
 
 - text <string> - text of error
-
 */
 func makeLogString(level, lc, text string) string {
 	now := time.Now().Format(timeFormat)
 	logPos := getLogPosition()
 	return fmt.Sprint(levelColors[level],
-		"[", loggerCfg.appName, "]", "[", now, "]", "[", lc, "]", "[", logPos, "]", "[", level, "]  ▶  ", text)
+		"[", loggerCfg.AppName, "]", "[", now, "]", "[", lc, "]", "[", logPos, "]", "[", level, "]  ▶  ", text)
+}
+
+/*
+makeLogJSONString - used to create a formatted JSON log string.
+
+- level <string> - level of logs
+
+- lc <string> - logging category
+
+- text <string> - text of error
+*/
+func makeLogJSONString(level, lc, text string) string {
+	logPos := s.Split(getLogPosition(), ":")
+	line, _ := strconv.Atoi(logPos[1])
+
+	JSONLog := JSONLog{
+		AppName: loggerCfg.AppName,
+		Time:    time.Now().Format(timeFormat),
+		LC:      lc,
+		File:    logPos[0],
+		Line:    line,
+		Level:   level,
+		Text:    text,
+	}
+
+	bLog, err := json.Marshal(JSONLog)
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprint(levelColors[level], string(bLog))
 }
 
 /*
@@ -183,19 +347,17 @@ func getLogPosition() string {
 
 /*
 SetConfig - configures the logging module for further work.
+	- config Cfg - contains:
 
-- appName <string> - name of the program.
+		- appName <string> - name of the program.
 
-- logPath <string> - path of the file for writing logs to it.
-If the variable is empty, then logs are not written to the file.
+		- logPath <string> - path of the file for writing logs to it.
+		If the variable is empty, then logs are not written to the file.
 
-- level <int> - logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
+		- level <int> - logger's level: 0 - DEBUG, 1 - INFO, 2 - WARNING, 3 - ERROR, 4 - CRITICAL
 */
-func SetConfig(appName, logPath string, level int) {
-	loggerCfg.appName = appName
-	loggerCfg.logPath = logPath
-	loggerCfg.level = level
-
+func SetConfig(config Cfg) {
+	loggerCfg = config
 	checkConfig()
 }
 
@@ -203,12 +365,12 @@ func SetConfig(appName, logPath string, level int) {
 checkConfig - checks the config and reconfigures it if necessary.
 */
 func checkConfig() {
-	if loggerCfg.level < 0 || loggerCfg.level > 4 {
-		loggerCfg.level = 0
+	if loggerCfg.Level < 0 || loggerCfg.Level > 4 {
+		loggerCfg.Level = 0
 	}
 
-	if len(loggerCfg.logPath) > 0 {
-		logs, err := os.OpenFile(loggerCfg.logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if len(loggerCfg.LogPath) > 0 {
+		logs, err := os.OpenFile(loggerCfg.LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			return
 		}
